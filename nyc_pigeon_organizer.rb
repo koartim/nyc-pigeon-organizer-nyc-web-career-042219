762c8d91d44ba614b10a510159fa706549af636b
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-20 17:45:06 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |attribute, colors|
+    colors.each do |color,pigeons|
+      pigeons.each do |pigeon|
+        pigeon_hash[pigeon] ||= {}
+        pigeon_hash[pigeon][attribute] ||= []
+        pigeon_hash[pigeon][attribute] << color.to_s
+        binding.pry
+        end
+      end
+    end
+    pigeon_hash
+  end
+
+def build_pigeon_attribute_hash
+
+  # this will make the top level hash with the
+  # pigeons names that point to empty objects
+  # {
+  #   'Theo' => {},
+  #   'Alex' => {},
+  #   ...,
+  # }
 end

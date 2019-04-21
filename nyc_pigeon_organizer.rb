182a44ac460d8149e36aeefacb70b5228e523a9f
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-21 14:46:23 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,26 +1,63 @@
 require 'pry'
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(pigeon_data)
   pigeon_hash = {}
-  data.each do |attribute, colors|
-    colors.each do |color,pigeons|
+  pigeon_data.each do |category, attributes|
+    attributes.each do |attribute, pigeons|
       pigeons.each do |pigeon|
-        pigeon_hash[pigeon] ||= {}
-        pigeon_hash[pigeon][attribute] ||= []
-        pigeon_hash[pigeon][attribute] << color.to_s
-        binding.pry
+
+        add_attributes_to_pigeon(pigeon_hash, pigeon, attribute, category)
+      end
+    end
+  end
+  pigeon_hash
+end
+
+def add_attributes_to_pigeon(pigeon_hash, pigeon, attribute, category)
+  if !pigeon_hash[pigeon]
+    pigeon_hash[pigeon] = {}
+    pigeon_hash[pigeon][category] = []
+    pigeon_hash[pigeon][category] << attribute.to_s
+  elsif !pigeon_hash[pigeon][category]
+    pigeon_hash[pigeon][category] = []
+    pigeon_hash[pigeon][category] << attribute.to_s
+  else
+    pigeon_hash[pigeon][category] << attribute.to_s
+  end
+  pigeon_hash
+end
+
+def pigeon_names(pigeon_data)
+  pigeon_names = []
+  pigeon_data.each do |category, attributes|
+    attributes.each do |attribute, pigeons|
+      pigeons.each do |pigeon|
+        if !pigeon_names.include?(pigeon)
+          pigeon_names << pigeon
         end
       end
     end
-    pigeon_hash
   end
+  pigeon_names
+end
 
 def build_pigeon_attribute_hash
 
   # this will make the top level hash with the
   # pigeons names that point to empty objects
+  # {}
   # {
   #   'Theo' => {},
   #   'Alex' => {},
+  #   ...,
+  # }
+  # {
+  #   'Theo' => {:color = []},
+  #   'Alex' => {:color = []},
+  #   ...,
+  # }
+  # {
+  #   'Theo' => {:color = ['purple', 'grey']},
+  #   'Alex' => {:color = ['black', 'purple']},
   #   ...,
   # }
 end
